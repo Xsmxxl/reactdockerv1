7933ddfe1f32947f6806ed1039414fd29c1e5707
Xsmxxl <ismael-uchiha1@hotmail.com>
2022-07-13 12:47:07 -0500

    Add new page, resume

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import NavItem from 'react-bootstrap/NavItem'
-import NavLink from 'react-bootstrap/NavLink'
+import Nav from 'react-bootstrap/Nav';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ResumenUser from './pages/resumen'
 
 function App() {
   return (
     <div className="App">
-      <Navbar expand='lg' variant="light">
+      <Navbar bg="light" expand='lg' variant="light">
         <Container>
           <Navbar.Toggle aria-controls='responsive-navbar-nav' />
           <Navbar.Collapse>
             <Nav className="me-auto">
               <Nav.Link>Home</Nav.Link>
-              <NavLink>Otro</NavLink>
-              <NavLink>Si</NavLink>
+              <Nav.Link>Otro</Nav.Link>
+              <Nav.Link>Si</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <Container>
-        <p>
-          Actualizando
-        </p>
-      </Container>
+      <hr className='my-0 border border-dark'/>
+      <Row className='mt-0 mx-0' xs={12} md={12} >
+        <Col xs={12} md={3}>
+          <Nav className="flex-column">
+            <Nav.Link>Resumen</Nav.Link>
+            <Nav.Link>Actualizar mis datos</Nav.Link>
+          </Nav>
+        </Col>
+        <Col xs={12} md={9}>
+          <ResumenUser/>
+        </Col>
+      </Row>
+
     </div>
   );
 }
